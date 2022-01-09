0f6b6cf50ac763562df2b0004ac4647adb1160e2
Himangshu Gorai <him.bcet@gmail.com>
2022-01-09 22:14:58 +0530

    My functional interface commit.

--- src/com/code/java8/java8.java
+++ src/com/code/java8/java8.java
@@ -5,6 +5,6 @@ package com.code.java8;
  */
 public class java8 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello functional interface...");
     }
 }
