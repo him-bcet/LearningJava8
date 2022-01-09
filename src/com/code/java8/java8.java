351bb0376712b898637fe2536b4ab7bf49ea8545
Himangshu Gorai <him.bcet@gmail.com>
2022-01-09 22:03:58 +0530

    My first commit

--- src/com/code/java8/java8.java
+++ src/com/code/java8/java8.java
@@ -4,4 +4,7 @@ package com.code.java8;
  * Created by Himangshu Gorai on 1/9/2022.
  */
 public class java8 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
